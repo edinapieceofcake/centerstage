40d651e4471e13319e0253f3b99f511e51db3904
Shishir Ghate <Shishir.Ghate@dowjones.com>
2024-03-26 08:05:36 -0500

    Fix initial setting of servo after restart. Added dropoffposition to dropoffpixel

--- TeamCode/src/main/java/edu/edina/library/util/PoCServo.java
+++ TeamCode/src/main/java/edu/edina/library/util/PoCServo.java
@@ -5,16 +5,18 @@ import com.qualcomm.robotcore.hardware.ServoImplEx;
 public class PoCServo {
     private ServoImplEx servo;
     private double currentPosition;
+    private boolean positionSetOnce = false;
     public PoCServo (ServoImplEx servo) {
         this.servo = servo;
         currentPosition = servo.getPosition();
     }
 
     public void setPosition(double position) {
-        //if (currentPosition != position) {
+        if (!positionSetOnce || (currentPosition != position)) {
             currentPosition = position;
             servo.setPosition(currentPosition);
-        //}
+            positionSetOnce = true;
+        }
     }
 
     public double getPosition() {
