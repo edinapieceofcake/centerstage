df82a08d3fc85b96b005261c0607ceb0b88d25ef
AlexanderBecker13 <144349466+AlexanderBecker13@users.noreply.github.com>
2024-03-28 17:36:22 -0500

    Copyed and Re-Factored Red Audience to Blue Audience1

---
+++ TeamCode/src/main/java/edu/edina/opmodes/auto/BlueAudience1.java
@@ -0,0 +1,97 @@
+package edu.edina.opmodes.auto;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+
+import edu.edina.library.enums.Alliance;
+import edu.edina.library.enums.PropLocation;
+
+public class BlueAudience1 extends Audience {
+
+    @Override
+    protected Alliance getAlliance() {
+            return Alliance.Blue;
+        }
+
+    @Override
+    protected Pose2d getStartPose() { return new Pose2d(-42, 64, Math.toRadians(270)); }
+
+    @Override
+    protected PropLocation getNonePropLocation() { return PropLocation.Left; }
+
+    @Override
+    protected void dropPurplePixel() {
+        Vector2d propDropLocation;
+        double propAngle = 270;
+
+        // Determine location for purple pixel
+        switch(propLocation) {
+            case Left:
+                propDropLocation = new Vector2d(-31, 35);
+                propAngle = 315;
+                backdropDropLocation = new Vector2d(50,44.5);
+                backdropDropLocationSecond = new Vector2d(48, 38);
+                backdropDropLocationAW = new Vector2d(48,43.5);
+                backdropDropLocationAW2 = new Vector2d(48, 33);
+                break;
+            case Right:
+                propDropLocation = new Vector2d(-39, 31);
+                propAngle = 225;
+                backdropDropLocation = new Vector2d(50,32);
+                backdropDropLocationAW = new Vector2d(48,27);
+                break;
+            case Center:
+            default:
+                propDropLocation = new Vector2d(-33, 34.5);  // default to Center if all goes bad
+                propAngle = 270;
+                backdropDropLocation = new Vector2d(50,38 ); // default to center if all goes bad
+                backdropDropLocationAW = new Vector2d(48, 35.5);
+                break;
+        }
+
+        // Run to drop PURPLE pixel
+        Actions.runBlocking(
+                new SequentialAction(
+                        drive.actionBuilder(drive.pose)
+                                .splineTo(propDropLocation, Math.toRadians(propAngle))
+                                .build(),
+                        manager.openLeftClaw(),
+                        manager.openAutoClaw()
+                )
+        );
+    }
+
+    @Override
+    protected void park() {
+        // park
+        switch (parkLocation) {
+            case Center:
+                Actions.runBlocking(
+                        new SequentialAction(
+                        drive.actionBuilder(drive.pose)
+                                .setReversed(true)
+                                .splineTo(new Vector2d(58, 14), Math.toRadians(0))
+                                .build(),
+                        manager.getLiftReadyToDrive()
+                        ));
+                break;
+            case Corner:
+                Actions.runBlocking(
+                        new SequentialAction(
+                        drive.actionBuilder(drive.pose)
+                                .setReversed(true)
+                                .splineTo(new Vector2d(58, 60), Math.toRadians(0))
+                                .build(),
+                        manager.getLiftReadyToDrive()
+                ));
+                break;
+            default:
+                Actions.runBlocking(new SequentialAction(
+                    manager.getLiftReadyToDrive()
+                ));
+                break;
+        }
+    }
+}
