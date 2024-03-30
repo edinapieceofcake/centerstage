4f975f7c196b7b8bb9049def584134ab203cdf12
Shishir Ghate <Shishir.Ghate@dowjones.com>
2024-03-30 12:40:12 -0500

    Added pixel pusher sensor

---
+++ TeamCode/src/main/java/edu/edina/opmodes/test/TestPixelPusher.java
@@ -0,0 +1,37 @@
+package edu.edina.opmodes.test;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import edu.edina.library.util.SmartGamepad;
+
+@TeleOp
+//@Disabled
+public class TestPixelPusher extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        SmartGamepad pad1 = new SmartGamepad(gamepad1);
+        Servo pixelMover = hardwareMap.get(Servo.class, "pixelLiftServo");
+
+        waitForStart();
+
+        pixelMover.setPosition(.5);
+        while (opModeIsActive()) {
+            pad1.update();
+
+            if (pad1.left_bumper) {
+                pixelMover.setPosition(pixelMover.getPosition() + .01);
+            }
+
+            if (pad1.right_bumper) {
+                pixelMover.setPosition(pixelMover.getPosition() - .01);
+            }
+
+            telemetry.addData("Bumpers control ths position", "left increases, right decreases");
+            telemetry.addData("PixelPusher position", pixelMover.getPosition());
+            telemetry.update();
+        }
+    }
+}
